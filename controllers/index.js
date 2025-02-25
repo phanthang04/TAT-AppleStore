23af3d3f77a29aadde484a7db3a4b4892aa500fe
PT tap Code <phanthangcht@gmail.com>
2025-02-25 20:51:34 +0700

    ok fen

---
+++ controllers/index.js
@@ -0,0 +1,10 @@
+// Xử lý Slide Banner
+
+const listBanner = document.querySelector(".list-banner")
+const banner = document.querySelectorAll(".banners")
+
+// console.log(banner)
+setInterval(() => {
+    let width = banner[0].offsetWidth
+    console.log.apply(width)
+},4000)
