094ce67ca0e401df38a9fec407735746b40c2d2a
PT tap Code <phanthangcht@gmail.com>
2025-02-26 01:14:04 +0700

    Update

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,50 @@
-// Xử lý Slide Banner
 
-const listBanner = document.querySelector(".list-banner")
-const banner = document.querySelectorAll(".banners")
+const listImage = document.querySelector('.list-banner')
+const imgs = document.querySelectorAll('banners')
+const btnLeft = document.querySelector('.btn-left')
+const btnRight = document.querySelector('.btn-right')
+const length = imgs.length
+let current = 0
 
-// console.log(banner)
-setInterval(() => {
-    let width = banner[0].offsetWidth
-    console.log.apply(width)
-},4000)
+const handleChangeSlide = () => {
+    if (current == length - 1) {
+        current = 0
+        let width = imgs[0].offsetWidth
+        listImage.style.transform = `translateX(0px)`
+        document.querySelector('.active').classList.remove('active')
+        document.querySelector('.index-item-'+ current).classList.add('active')
+
+    } else {
+        current ++
+        let width = imgs[0].offsetWidth
+        listImage.style.transform = `translateX(${width * -1 * current}px)`
+        document.querySelector('.active').classList.remove('active')
+        document.querySelector('.index-item-'+ current).classList.add('active')
+    }
+}
+
+let handleEventChangeSlide = setInterval(handleChangeSlide, 4000)
+
+btnRight.addEventListener('click', () => {
+    clearInterval(handleEventChangeSlide)
+    handleChangeSlide()
+    handleEventChangeSlide = setInterval(handleChangeSlide, 4000)
+})
+
+btnLeft.addEventListener('click', () => {
+    clearInterval(handleEventChangeSlide)
+    if (current == 0) {
+        current = length - 1
+        let width = imgs[0].offsetWidth
+        listImage.style.transform = `translateX(${width * -1 * current}px)`
+        document.querySelector('.active').classList.remove('active')
+        document.querySelector('.index-item-'+ current).classList.add('active')
+    } else {
+        current --
+        let width = imgs[0].offsetWidth
+        listImage.style.transform = `translateX(${width * -1 * current}px)`
+        document.querySelector('.active').classList.remove('active')
+        document.querySelector('.index-item-'+ current).classList.add('active')
+    }
+    handleEventChangeSlide = setInterval(handleChangeSlide, 4000)
+})
